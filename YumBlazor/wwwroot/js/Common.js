4955897f43ee2336cf057a944cbcd0e4376be70f
TsubasaWoKudasai <jlkramek@gmail.com>
2025-01-02 17:54:11 -0300

    bsModal

---
+++ YumBlazor/wwwroot/js/Common.js
@@ -0,0 +1,10 @@
+﻿function ShowConfirmationModal()
+{
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).show();
+}
+
+function HideConfirmationModal()
+{
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).hide();
+}
+
