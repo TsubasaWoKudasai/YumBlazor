4d7acbde68cafa7e8f05a31d90a17ee849afa6d4
TsubasaWoKudasai <jlkramek@gmail.com>
2025-01-03 15:08:29 -0300

    Product List Ui

--- YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 
 namespace YumBlazor.Data
--- YumBlazor/Data/Product.cs
+++ YumBlazor/Data/Product.cs
@@ -1,21 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace YumBlazor.Data
 {
     public class Product
     {
-        public int Id {  get; set; }
-
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-
-        [Range(0.001, 1000)]
+        [Range(0.01, 1000)]
         public decimal Price { get; set; }
         public string? Description { get; set; }
         public string? SpecialTag { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
 
-        public string ? ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
+
     }
 }
